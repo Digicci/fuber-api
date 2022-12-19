d7d34076a8719ef96daa52e683e844ab6d9bc64c
Brasero <brandonricci4@gmail.com>
2022-12-19 09:07:19 +0100

    adding controllers folder and migrating part of user logique

--- router/routes/user.js
+++ router/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();    // Create a router
 const db = require('../../models/index');
+const { createUser } = require('../../controllers/userController/index');
 
 router.post('/login', function (req, res) {
     const { email, password } = req.body
@@ -17,42 +18,6 @@ router.post('/login', function (req, res) {
     }
 })
 
-router.post('/signup', function (req, res) {
-    const { email, mdp, nom, prenom, tel } = req.body
-    if (email === '' || mdp === '' || nom === '' || prenom === '' || tel === '') {
-        res.status(400).send('Bad request.')
-    }   else {
-        const utilisateur = db['utilisateur']
-        utilisateur.findAll({
-            attributes: ['mail'],
-        }).then((users) => {
-            users.forEach((user) => {
-                if (user.mail === email) {
-                    res.status(400).send('Already exists.')
-                }
-            })
-        })
-        utilisateur.create({
-            mail : email,
-            mdp : mdp,
-            nom : nom,
-            prenom : prenom,
-            num : tel,
-            adresse : '',
-            ville : '',
-            cp : '',
-            pays : '',
-            code_recup : '',
-            createdAt : new Date(),
-            updatedAt : new Date()
-        }).then((user) => {
-            if (user) {
-                res.status(201).send('true')
-            }  else {
-                res.status(400).send('false')
-            }
-        })
-    }
-})
+router.post('/signup', createUser)
 
 module.exports = router
